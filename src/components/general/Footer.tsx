d1d98c08b17a54bda342303f367bd4ae6d77344a
bryamslm <bryamlopez4@gmail.com>
2025-06-24 17:26:20 -0600

    unused icon

--- src/components/general/Footer.tsx
+++ src/components/general/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Facebook, Instagram, Phone, Mail, MessageCircle, Youtube, Heart, MessageSquare } from 'lucide-react';
+import { Facebook, Instagram, Phone, Mail, MessageCircle, Youtube, Heart } from 'lucide-react';
 
 export default function Footer({ locale }: { locale: string }) {
   return (
