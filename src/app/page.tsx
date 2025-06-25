9d5af5e67a3c44958d9db353dcb1d0822fc099ec
bryamslm <bryamlopez4@gmail.com>
2025-06-25 00:56:40 -0600

    console host

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { headers } from 'next/headers';//obtener el host
 export default async function RootPage() {
   const h = await headers();
   const host  = h.get('host');              // handmadeart.store o artehechoamano.com
+  console.log("El host es:", host);
   if (host === 'artehechoamano.com'){
     redirect('/es');
   }
