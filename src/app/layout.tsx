3970a2f43f11e89893786d9cac0afed53d9ab2ab
bryamslm <bryamlopez4@gmail.com>
2025-06-25 01:48:49 -0600

    a ve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,19 @@ import { getCommonMetadata } from "@/lib/seo";
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
 import { Suspense } from 'react'
+import { headers } from "next/headers";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const h = await headers();
+  const host = h.get('x-forwarded-host') ?? h.get('host');
+  return {
+    title: {
+      default: "HandMade Art",
+      template: '%s | HandMade Art'
+    },
+    ...getCommonMetadata(host === 'artehechoamano.com' ? 'es' : 'en'),
+  };
+};
 
 export default function RootLayout({
   children,
