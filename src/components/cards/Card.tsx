b981b6d1cac4d278ac9179d5ce69ffac6cc596cd
bryamslm <bryamlopez4@gmail.com>
2025-06-29 01:34:49 -0600

    mm

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -13,7 +13,7 @@ const Card: React.FC<CardProps> = ({ title, content, link }) => (
   <div className="relative bg-[#303030]  rounded-xs overflow-hidden starting: h-full max-w-full w-full  hover:shadow-sm transition-shadow">
     <div className="flex flex-col h-full">
       <div className="px-3 bg-gradient-to-b from-[#303030] to-transparent">
-        <Link href={link} className="block">
+        <Link href={link} className="block" target="_self">
           <h3 className="text-lg font-semibold text-white hover:text-teal-600 transition-colors">{title}</h3>
         </Link>
       </div>
