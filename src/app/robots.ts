6b6c97b23b6d20eed2bed924fec05891865b2a80
bryamslm <bryamlopez4@gmail.com>
2025-07-04 20:13:51 -0600

    sisi

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,11 @@
 import type { MetadataRoute } from "next";
 
+// /account es privada asi como admin
+
 export default async function robots(): Promise<MetadataRoute.Robots> {
 
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: { userAgent: "*", allow: "/", disallow: ["/account", "/admin", "/auth", "/api"] },
     // Declaramos *ambos* sitemaps para reforzar la relación entre dominios
     sitemap: [
       `https://sobrepoxi.com/sitemap.xml`,
