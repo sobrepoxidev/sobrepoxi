0bdb47171332137cc163221c1d0ab9601058101b
bryamslm <bryamlopez4@gmail.com>
2025-07-04 20:14:05 -0600

    ss

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ import type { MetadataRoute } from "next";
 export default async function robots(): Promise<MetadataRoute.Robots> {
 
   return {
-    rules: { userAgent: "*", allow: "/", disallow: ["/account", "/admin", "/auth", "/api"] },
+    rules: { userAgent: "*", allow: "/", disallow: ["/account", "/admin", "/auth"] },
     // Declaramos *ambos* sitemaps para reforzar la relación entre dominios
     sitemap: [
       `https://sobrepoxi.com/sitemap.xml`,
